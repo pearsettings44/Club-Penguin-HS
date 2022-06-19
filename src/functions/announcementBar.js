50f06c6d25256b5909f9bf0e1369307985c5e84b
pearsettings44 <jose.a.pereira03@gmail.com>
2022-06-19 18:04:03 +0100

    add navbar

--- src/functions/announcementBar.js
+++ src/functions/announcementBar.js
@@ -4,10 +4,13 @@ import { maindiv } from "../index";
 function renderAnnouncementBar() {
   const content = "CLICK TO WATCH THE BAKER VIDEO WITH TYSON AND T-FUNK";
   const video = "https://www.youtube.com/watch?v=dGCopkpYuOg&t=12s";
+
   const header = createHTMLelement("header", "header", "", "");
   const header_content = createHTMLelement("a", "header_video", "", content);
+
   header_content.href = video;
   header.appendChild(header_content);
+  /* Append to the DOM */
   maindiv.appendChild(header);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { renderAnnouncementBar } from "./functions/announcementBar";
+import { renderNav } from "./functions/navbar";
 
 /* Main div, contains everything */
 const maindiv = document.getElementById("content");
 renderAnnouncementBar();
+renderNav();
 export { maindiv };
